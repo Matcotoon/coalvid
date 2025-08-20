9a8164a97c73873c3520a8dbe294c3ff0970ea15
Matcotoon <dvaldivieso6@gmail.com>
2025-08-20 17:38:35 -0500

    Segundo Commit

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,16 @@
 <?php 
 
 use Model\ActiveRecord;
+
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+// Cargar variables del .env desde la raíz del proyecto
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
+// Funciones y configuración de la base de datos
 require 'funciones.php';
 require 'config/database.php';
 
-
+// Asignar conexión de base de datos a ActiveRecord
 ActiveRecord::setDB($db);
